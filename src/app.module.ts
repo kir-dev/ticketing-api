58bd73e57155f015b04e3a625f0c5ffacb8d3782
triszt4n <trisztanpiller@gmail.com>
2022-03-07 21:45:30 +0100

    Add env vars and configure mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,10 @@ import { AppService } from './app.service'
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
     ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@ticketcluster.ssxvz.mongodb.net/ticketing?retryWrites=true&w=majority`,
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
