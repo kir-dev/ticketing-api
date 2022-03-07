e414a72b05888a3c59649d71dc9399cc9103d510
triszt4n <trisztanpiller@gmail.com>
2022-03-08 00:20:20 +0100

    Getting rid of app controller and stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { TicketsModule } from './tickets/tickets.module'
 
 @Module({
@@ -13,7 +11,5 @@ import { TicketsModule } from './tickets/tickets.module'
     ),
     TicketsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
