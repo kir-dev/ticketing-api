b4c5989f72f1cf1e574518c5d65b23acd4cc831d
triszt4n <trisztanpiller@gmail.com>
2022-03-07 22:08:33 +0100

    Add ticket resource with commandline

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { TicketsModule } from './tickets/tickets.module'
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AppService } from './app.service'
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@ticketcluster.ssxvz.mongodb.net/ticketing?retryWrites=true&w=majority`,
     ),
+    TicketsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/tickets/tickets.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common'
+import { CreateTicketDto } from './dto/create-ticket.dto'
+import { UpdateTicketDto } from './dto/update-ticket.dto'
+import { TicketsService } from './tickets.service'
+
+@Controller('tickets')
+export class TicketsController {
+  constructor(private readonly ticketsService: TicketsService) {}
+
+  @Post()
+  create(@Body() createTicketDto: CreateTicketDto) {
+    return this.ticketsService.create(createTicketDto)
+  }
+
+  @Get()
+  findAll() {
+    return this.ticketsService.findAll()
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ticketsService.findOne(+id)
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTicketDto: UpdateTicketDto) {
+    return this.ticketsService.update(+id, updateTicketDto)
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.ticketsService.remove(+id)
+  }
+}
