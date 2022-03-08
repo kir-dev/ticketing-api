65be53e39a26543a785a8643c3dc79732c67919d
triszt4n <trisztanpiller@gmail.com>
2022-03-08 17:57:23 +0100

    Add swagger with ui

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
   app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+    .setTitle('Ticketing')
+    .setDescription('The Ticketing API description')
+    .setVersion('1.0')
+    .addTag('support')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.APP_PORT || 3000)
 }
 bootstrap()
