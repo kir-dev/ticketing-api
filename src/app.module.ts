debea918ae44c4678070773ed2fa492a4c884201
triszt4n <trisztanpiller@gmail.com>
2022-03-08 14:58:58 +0100

    Better env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
-import { TicketsModule } from './tickets/tickets.module'
 import { BoardsModule } from './boards/boards.module'
 import { LabelsModule } from './labels/labels.module'
+import { TicketsModule } from './tickets/tickets.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@ticketcluster.ssxvz.mongodb.net/ticketing?retryWrites=true&w=majority`,
+      `mongodb+srv://
+        ${process.env.DATABASE_USERNAME}:
+        ${process.env.DATABASE_PASSWORD}@
+        ${process.env.DATABASE_HOST}/
+        ${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
     ),
     TicketsModule,
     BoardsModule,
